462945eda3a04b6fc05142a7d2d5da7e94dbf334
Fabrice <fab.hatege15@gmail.com>
2023-11-26 21:55:10 +0100

    Implemented the functions according to what I understand. Should find out how to represent the data

--- tp6/app.py
+++ tp6/app.py
@@ -1,21 +1,28 @@
 import numpy as np
 import pandas as pd
+import random
 
 
 def initial_values(size):
     return np.random.choice([0, 1], size=size)
 
 
-def fitness(x):
+def mapping(a, b):
+    # m number of bits of the sequence
+    return lambda x: (x/(2 ** len(x))) * (b-a) + a
+
+
+def compute_fitness(x):
     return 1
 
 
-def get_survivors(df):
-    return df
-
-
 def crossover(row):
-    return row
+    parent1, parent2 = row["individual1"], row["individual2"]
+    columns = ["individual1", "individual2"]
+    point = random.randint(0, len(parent1)-1)
+    child1 = parent1[:point] + parent2[point:]
+    child2 = parent2[:point] + parent1[point:]
+    return pd.DataFrame([child1, child2], columns=columns)
 
 
 def selection(df, algo):
@@ -29,16 +36,21 @@ def get_mate(individuals):
                         columns=["individual1", "individual2"])
 
 
-def mutation(params):
-    pass
+def mutation(individual):
+    point = random.randint(0, len(individual)-1)
+    individual[point] = 1 if individual[point] == 0 else 0
+    return individual
 
 
 def choose5(individuals):
-    pass
+    shuffled = random.shuffle(individuals)
+    return shuffled[:5], shuffled[5:]
 
 
-def get_best(candidate):
-    pass
+def get_best(candidates):
+    fitness = [compute_fitness(c) for c in candidates]
+    return sorted(zip(candidates, fitness),
+                  key=lambda x: x[1])[0][1]
 
 
 def tournament5_helper(individuals):
@@ -62,10 +74,11 @@ def genetic_algorithm(N, size, G):
     df = pd.DataFrame([initial_values(size).__str__() for i in range(N)],
                       columns=["individual"])
     for i in range(G):
-        df["fitness"] = df["individual"].apply(fitness)
-        mates = (get_mate(selection(df, tournament5(N//2)))
-                 .apply(crossover, axis=1))
-        df = pd.DataFrame((mates["individual1"] + mates["individual2"])
+        df["fitness"] = df["individual"].apply(compute_fitness)
+        mates = get_mate(selection(df, tournament5(N//2)))
+        childs = mates.apply(crossover, axis=1)
+        df = pd.concat([mates, childs], ignore_index=True)
+        df = pd.DataFrame((df["individual1"] + df["individual2"])
                           .apply(mutation), columns=["individual"])
     return df
 
