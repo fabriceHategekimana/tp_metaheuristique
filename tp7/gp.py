1caf528713d092dac7476e1e445aecba4965cb55
Fabrice <fab.hatege15@gmail.com>
2024-01-01 19:33:10 +0100

    Started the project

--- tp7/gp.py
+++ tp7/gp.py
@@ -1,83 +1,100 @@
+from typing import Callable
 import random
+
+
+def do_equal(val1, val2):
+    if val1 is None or val2 is None:
+        return 0
+    else:
+        return val1 == val2
 
 
 # This is the machine on which programs are executed
 # The output is the value on top of the pile.
 class CPU:
     def __init__(self):
-        self.pile = []
+        self.pile: list[int] = []
 
     def reset(self):
         while len(self.pile) > 0:
             self.pile.pop()
 
-    def push(self, val):
+    def push(self, val: int):
         self.pile.append(val)
 
-    def pop(self):
+    def pop(self) -> int | None:
         return self.pile.pop() if len(self.pile) > 0 else None
 
 
 # These are the instructions
-def AND(cpu, data):
+def AND(cpu: CPU, data: list[int]):
     v1 = cpu.pop()
     v2 = cpu.pop()
-    cpu.push(v1 and v2)
+    if v1 is not None and v2 is not None:
+        v1i: int = v1
+        v2i: int = v2
+        cpu.push(v1i and v2i)
 
 
-def OR(cpu, data):
+def OR(cpu: CPU, data: list[int]):
     v1 = cpu.pop()
     v2 = cpu.pop()
-    cpu.push(v1 or v2)
+    if v1 is not None and v2 is not None:
+        v1i: int = v1
+        v2i: int = v2
+        cpu.push(v1i or v2i)
 
 
-def XOR(cpu, data):
+def XOR(cpu: CPU, data: list[int]):
     v1 = cpu.pop()
     v2 = cpu.pop()
     cpu.push((v1 and v2) or (not (v1 or v2)))
 
 
-def NOT(cpu, data):
+def NOT(cpu: CPU, data: list[int]):
     v = cpu.pop()
     cpu.push(not v)
 
 
 # Push values of variables on the stack.
-def X1(cpu, data):
+def X1(cpu: CPU, data: list[int]):
     cpu.push(data[0])
 
 
-def X2(cpu, data):
+def X2(cpu: CPU, data: list[int]):
     cpu.push(data[1])
 
 
-def X3(cpu, data):
+def X3(cpu: CPU, data: list[int]):
     cpu.push(data[2])
 
 
-def X4(cpu, data):
+def X4(cpu: CPU, data: list[int]):
     cpu.push(data[3])
 
 
 # Execute a program
-def execute(program, cpu, data):
-    pass
+def execute(program: list[str], cpu: CPU, data: list[int]) -> int | None:
+    for cmd in program:
+        CALLS[cmd](cpu, data)
+    return cpu.pop()
 
 
 # Generate a random program
-def randomProg(length, functionSet, terminalSet):
+def randomProg(length: int, functionSet: list[str], terminalSet: list[str]) -> list[str]:
     return [random.choice(functionSet+terminalSet) for i in range(length)]
 
 
 # Computes the fitness of a program.
-# The fitness counts how many instances of data in dataSet are correctly computed by the program
-def computeFitness(prog, cpu, dataSet):
-    # TO DO
-    pass
+# The fitness counts how many instances of data in dataSet are correctly
+# computed by the program
+def computeFitness(prog: list[str], cpu: CPU, dataSet: list[list[int]]) -> int:
+    return sum([do_equal(execute(prog, cpu, line[:-1]), line[-1])
+                for line in dataSet])
 
 
 # Selection using 2-tournament.
-def selection(Population, cpu, dataSet):
+def selection(Population, cpu: CPU, dataSet):
     listOfFitness = []
     for i in range(len(Population)):
         prog = Population[i]
@@ -132,14 +149,16 @@ def mutation(Population, p_m, terminalSet, functionSet):
     return newPopulation
 
 
-calls = {"AND": AND, "OR": OR, "XOR": XOR, "NOT": NOT,
-         "X1": X1, "X2": X2, "X3": X3, "X4": X4}
+CALLS: dict[str, Callable] = {"AND": AND, "OR": OR,
+                              "XOR": XOR, "NOT": NOT,
+                              "X1": X1, "X2": X2,
+                              "X3": X3, "X4": X4}
 
 # -------------------------------------
 
 # LOOK-UP TABLE YOU HAVE TO REPRODUCE.
 nbVar = 4
-dataSet = [
+dataSet: list[list[int]] = [
     [0, 0, 0, 0, 0],
     [0, 0, 0, 1, 1],
     [0, 0, 1, 0, 0],
@@ -158,30 +177,43 @@ dataSet = [
     [1, 1, 1, 1, 0],
 ]
 
-cpu = CPU()
+cpu: CPU = CPU()
 
 # Function and terminal sets.
-functionSet = ["AND", "OR", "NOT", "XOR"]
-terminalSet = ["X1", "X2", "X3", "X4"]
+functionSet: list[str] = ["AND", "OR", "NOT", "XOR"]
+terminalSet: list[str] = ["X1", "X2", "X3", "X4"]
 
 # Example of program.
-prog = ["X1", "X2", "AND", "X3", "OR"]
-progLength = 5
+prog: list[str] = ["X1", "X2", "AND", "X3", "OR"]
+progLength: int = 5
+# Generator
 prog = randomProg(progLength, functionSet, terminalSet)
 
+# fitness
 print(prog)
 
 # Execute a program on one row of the data set.
-data = dataSet[0]
-output = execute(prog, cpu, data)
+data: list[int] = dataSet[0]
+output: int | None = execute(prog, cpu, data)
 print(output)
 print("-------------")
 
-# # Parameters
+# Parameters
 # popSize = None  # TODO
 # p_c = None  # TODO
 # p_m = None  # TODO
-# 
-# # Generate the initial population
-# 
-# # Evolution. Loop on the creation of population at generation i+1 from population at generation i, through selection, crossover and mutation.
+# Generate the initial population
+# Evolution. Loop on the creation of population at generation i+1 from population at generation i, through selection, crossover and mutation.
+
+# TODO
+# generate a population
+prog = randomProg(progLength, functionSet, terminalSet)
+
+# calcule le fitness
+score = computeFitness(prog, cpu, dataSet)
+
+# selectionne les survivants
+
+
+# crossover les parents
+# mutation
---
+++ tp7/repport.md
@@ -0,0 +1,41 @@
+Bien sûr, je peux vous fournir un exemple de rapport scientifique sur l'étude de l'algorithme génétique classique, en incluant des graphiques fictifs pour illustrer les résultats. Veuillez noter que cet exemple est purement fictif et ne repose sur aucune recherche réelle.
+
+---
+
+# Rapport Scientifique sur l'Étude de l'Algorithme Génétique Classique
+
+## Introduction
+
+Les algorithmes génétiques (AG) sont des méthodes d'optimisation inspirées du processus de sélection naturelle. Ils sont largement utilisés dans divers domaines pour résoudre des problèmes d'optimisation complexe. Cette étude vise à analyser les performances de l'algorithme génétique classique dans la résolution du problème [insérer le problème spécifique], en examinant l'impact de différentes configurations sur la convergence et la qualité des solutions obtenues.
+
+## Méthodologie
+
+### Paramètres de l'Algorithme Génétique
+
+Nous avons utilisé un ensemble de paramètres standard pour l'algorithme génétique classique, comprenant une population de [taille de la population], une probabilité de croisement de [probabilité de croisement], une probabilité de mutation de [probabilité de mutation], etc.
+
+### Problème de Test
+
+Le problème choisi pour cette étude est [décrire brièvement le problème]. Il s'agit d'un problème couramment utilisé pour évaluer les performances des algorithmes d'optimisation.
+
+## Résultats
+
+### Convergence de l'Algorithme
+
+[graphique] [Insérer un graphique montrant la convergence de l'algorithme génétique au fil des générations. L'axe des x représente le nombre de générations, et l'axe des y représente la valeur de l'objectif.]
+
+L'analyse du graphique de convergence indique que l'algorithme atteint une convergence satisfaisante après [nombre de générations]. Cela confirme l'efficacité de l'algorithme génétique dans la résolution du problème de test.
+
+### Sensibilité aux Paramètres
+
+[graphique] [Insérer un graphique montrant comment la performance de l'algorithme varie en fonction de certains paramètres, par exemple, la taille de la population ou la probabilité de mutation.]
+
+L'analyse de la sensibilité aux paramètres révèle que la taille de la population influence significativement la rapidité de convergence, tandis que la probabilité de mutation a un impact sur la diversité génétique. Ces informations sont cruciales pour choisir des paramètres optimaux en fonction des exigences spécifiques du problème.
+
+## Conclusion
+
+Cette étude a démontré que l'algorithme génétique classique est efficace pour résoudre le problème [insérer le problème]. Les résultats indiquent une convergence rapide et une sensibilité aux paramètres, soulignant l'importance de l'ajustement des paramètres en fonction des caractéristiques du problème. Des recherches ultérieures pourraient explorer des variantes de l'algorithme ou des stratégies d'optimisation spécifiques pour améliorer davantage les performances.
+
+---
+
+N'oubliez pas d'ajuster le contenu en fonction de votre étude spécifique et des résultats que vous pourriez obtenir.
